55fe50d86ab503babdb1b94abdfe3a4e99330ba4
ttsd <angelwrialm48@gmail.com>
2026-01-08 04:44:23 +0800

    eg

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -6,6 +6,7 @@ export const useAuth = () => {
 	const context = useContext(AuthContext);
 	if (!context) throw new Error("useAuth must be used within an AuthProvider");
 	return context;
+	//comment
 };
 
 export const AuthProvider = ({ children }) => {
